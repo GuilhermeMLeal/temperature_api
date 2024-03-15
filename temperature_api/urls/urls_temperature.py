8ba1b250b7ada65b0d719760c8a72f4791e9b2e5
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-03-14 22:58:20 -0300

    fix(urls): fixed urls implementation

--- temperature_api/urls/urls_temperature.py
+++ temperature_api/urls/urls_temperature.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-#from temperature_api.views.weather_view import WeatherViewSet
+from temperature_api.views.weather_view import WeatherView
 
 
-router = DefaultRouter()
 
-#router.register(r'data_weather', WeatherViewSet)
-
+# Incluir as URLs do router no urlpatterns
 urlpatterns = [
-    path('', include(router.urls))
+    
 ]
+
