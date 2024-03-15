2f8a642241d3de1057fc07e99c8f4c0a96a71df9
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-03-14 22:59:09 -0300

    feat(views): implemented views in model

--- temperature_api/views/weather_view.py
+++ temperature_api/views/weather_view.py
@@ -5,14 +5,21 @@ from django.views import View
 from django.http import HttpResponse
 from temperature_api.models.weather_model import WeatherEntity
 
+
 class WeatherView(View):
     def get(self, request):
-        weathers = []
-        for i in range(10):
-            weathers.append(
-                WeatherEntity(
-                    temperature=randrange(start=17, stop=40),
-                    date=datetime.now()
-                )
-            )
-        return render(request, "html/home_data.html", {"weathers": weathers})
+        repository = WeatherRepository(collectionName='weathers')
+        weathers = repository.getAll()
+        return render(request, "home.html", {"weathers":weathers})
+    
+
+class WeatherGenerate(View):
+    def get(self, request):
+        repository = WeatherRepository(collectionName='weathers')
+        wheater = {
+            "temperature" : 28,
+            "date": "hoje"
+            }
+        repository.insert(wheater)
+
+        return redirect('Weather View')
