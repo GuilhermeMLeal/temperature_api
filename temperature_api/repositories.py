8b66ae966eaa71358a3edc1e72a027631d07a47a
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-03-19 22:26:22 -0300

    feat(repository): added repository to implement mongo for application

--- temperature_api/repositories.py
+++ temperature_api/repositories.py
@@ -1,46 +1,50 @@
-# repositories.py
 
 from django.conf import settings
 import pymongo
 
 class WeatherRepository:
-
-	collection = ''
-	
-	def __init__(self, collectionName) -> None:
-			self.collection = collectionName
-			
-	def getConnection(self):
-		  stringConnection = getattr(settings, "MONGO_CONNECTION_STRING")
-		  database = getattr(settings, "MONGO_DATABASE_NAME")
-			client = pymongo.MongoClient(stringConnection)
-			connection = client[database]
-			return connection
-			
-	def getCollection(self):
-			connection = self.getConnection()
-			collection = connection[self.collection]
-			return collection
-			
-	# CRUD
-	
-	
-	def getById(self):
-		pass
-		
-	def getAll(self):
-		document = self.getCollection().find({})
-		
-	def getByAttribute(self, attribute, value):
-		pass
-		
-	def insert(self, document) -> None:
-		self.getCollection().insert_one(document)
-		
-	def delete(self, document) -> None:
-		pass
-		
-	def deleteAll(self) -> None:
-		pass
-			
-	
+    collection = ''
+    
+    def __init__(self, collectionName) -> None:
+        self.collection = collectionName
+        
+    def getConnection(self):
+        stringConnection = getattr(settings, "MONGO_CONNECTION_STRING")
+        database = getattr(settings, "MONGO_DATABASE_NAME")
+        client = pymongo.MongoClient(stringConnection)
+        connection = client[database]
+        return connection
+        
+    def getCollection(self):
+        connection = self.getConnection()
+        collection = connection[self.collection]
+        return collection
+        
+    # CRUD
+    
+    def getById(self, id):
+        collection = self.getCollection()
+        document = collection.find_one({"_id": id})
+        return document
+        
+    def getAll(self):
+        collection = self.getCollection()
+        documents = collection.find({})
+        return list(documents)
+    
+    def getByAttribute(self, attribute, value):
+        collection = self.getCollection()
+        documents = collection.find({attribute: value})
+        return list(documents)
+    
+    def insert(self, document):
+        collection = self.getCollection()
+        collection.insert_one(document)
+    
+    def delete(self, id):
+        collection = self.getCollection()
+        collection.delete_one({"_id": id})
+    
+    def deleteAll(self):
+        collection = self.getCollection()
+        collection.delete_many({})
