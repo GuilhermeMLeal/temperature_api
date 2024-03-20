cace05729c1558d02ef3f1d40978a3a3b4d729b6
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-03-19 22:25:44 -0300

    feat(controller): implemented all functions for http methods

--- temperature_api/views/weather_view.py
+++ temperature_api/views/weather_view.py
@@ -1,25 +1,32 @@
 from datetime import datetime
 from random import randrange
-from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from temperature_api.models.weather_model import WeatherEntity
-
+from temperature_api.repositories import WeatherRepository
+from temperature_api.weatherSerializer import WeatherSerializer
 
 class WeatherView(View):
     def get(self, request):
         repository = WeatherRepository(collectionName='weathers')
         weathers = repository.getAll()
-        return render(request, "home.html", {"weathers":weathers})
-    
+        serializer = WeatherSerializer(weathers, many=True)
+        return render(request, "home_data.html", {"weathers": serializer.data})
 
 class WeatherGenerate(View):
-    def get(self, request):
+    def post(self, request):
         repository = WeatherRepository(collectionName='weathers')
-        wheater = {
-            "temperature" : 28,
-            "date": "hoje"
-            }
-        repository.insert(wheater)
+        weather_data = {
+            "temperature": randrange(start=17, stop=40),
+            "date": datetime.now()
+        }
+        serializer = WeatherSerializer(data=weather_data)
+        if serializer.is_valid():
+            repository.insert(serializer.validated_data)
+        return redirect('Weather View')
 
+class WeatherReset(View):
+    def delete(self, request):
+        repository = WeatherRepository(collectionName='weathers')
+        repository.deleteAll()
         return redirect('Weather View')
