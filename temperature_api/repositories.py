6ef9c46697404d622a4b7b6ea2fdaefa485a583d
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-03-20 21:18:13 -0300

    feat(repositories): implemented functions to db communication
    Insert, delete, get and connection methods

--- temperature_api/repositories.py
+++ temperature_api/repositories.py
@@ -1,50 +1,51 @@
-
 from django.conf import settings
 import pymongo
 
 class WeatherRepository:
+
     collection = ''
-    
+
     def __init__(self, collectionName) -> None:
         self.collection = collectionName
-        
+
     def getConnection(self):
         stringConnection = getattr(settings, "MONGO_CONNECTION_STRING")
         database = getattr(settings, "MONGO_DATABASE_NAME")
         client = pymongo.MongoClient(stringConnection)
         connection = client[database]
         return connection
-        
+
     def getCollection(self):
         connection = self.getConnection()
         collection = connection[self.collection]
         return collection
-        
+
     # CRUD
-    
-    def getById(self, id):
+
+    def getById(self, documentId):
         collection = self.getCollection()
-        document = collection.find_one({"_id": id})
+        document = collection.find_one({"_id": documentId})
         return document
-        
+
     def getAll(self):
         collection = self.getCollection()
         documents = collection.find({})
-        return list(documents)
-    
+        return documents
+
     def getByAttribute(self, attribute, value):
         collection = self.getCollection()
         documents = collection.find({attribute: value})
-        return list(documents)
-    
-    def insert(self, document):
+        return documents
+
+    def insert(self, document) -> None:
         collection = self.getCollection()
         collection.insert_one(document)
-    
-    def delete(self, id):
+
+    def delete(self, documentId) -> None:
         collection = self.getCollection()
-        collection.delete_one({"_id": id})
-    
-    def deleteAll(self):
+        collection.delete_one({"_id": documentId})
+
+    def deleteAll(self) -> None:
         collection = self.getCollection()
         collection.delete_many({})
+
