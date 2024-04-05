f504788938a09f21b74c7cb3438643d8ce8b65a8
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-04-04 23:02:50 -0300

    feat: implemented edit and remove buttons

--- temperature_api/views/weather_view.py
+++ temperature_api/views/weather_view.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from random import randrange
 from django.views import View
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from temperature_api.models.weather_model import WeatherEntity
 from temperature_api.repositories import WeatherRepository
 from temperature_api.weatherSerializer import WeatherSerializer
 from temperature_api.WeatherForm import WeatherForm
-
+from bson import ObjectId
+import uuid
 
 class WeatherView(View):
   def get(self, request):
@@ -25,7 +26,9 @@ class WeatherView(View):
 class WeatherGenerate(View):
     def get(self, request):
         repository = WeatherRepository(collectionName='weathers')
+        _id = str(uuid.uuid4())  # Gera um _id único
         weather = WeatherEntity(
+            _id=_id,
             temperature=randrange(start=17, stop=40),
             date=datetime.now()
         )
@@ -62,4 +65,24 @@ class WeatherInsert(View):
         else:
             print(weatherForm.errors)
 
+        return redirect('Weather View')
+
+class WeatherEdit(View):
+    def get(self, request, id):
+        weather = get_object_or_404(WeatherEntity, id=id)
+        form = WeatherForm(instance=weather)
+        return render(request, 'edit.html', {'form': form})
+
+    def post(self, request, id):
+        weather = get_object_or_404(WeatherEntity, id=id)
+        form = WeatherForm(request.POST, instance=weather)
+        if form.is_valid():
+            form.save()
+            return redirect('Weather View')
+        return render(request, 'edit.html', {'form': form})
+
+class WeatherRemove(View):
+    def get(self, request, id):
+        repository = WeatherRepository(collectionName='weathers')
+        repository.delete(id)
         return redirect('Weather View')
