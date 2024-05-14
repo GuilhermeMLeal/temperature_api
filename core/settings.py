3960082ff03a07ec423225bd231d8507f68838bc
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-05-14 19:31:33 -0300

    chore: updated to be recognized in chore

--- core/settings.py
+++ core/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
     'rest_framework',
     'temperature_api',
-    'rest_framework_simplejwt',
     'user',
 ]
 
