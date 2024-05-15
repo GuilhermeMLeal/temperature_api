c4c8b761706994117bb9b86b676fe7780b2482b1
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-05-14 22:20:26 -0300

    feat(weather model and serializer): implemented in weather app

--- temperature_api/weatherSerializer.py
+++ temperature_api/weatherSerializer.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-from temperature_api.models.weather_model import WeatherEntity 
-
+from .models import WeatherEntity
 
 class WeatherSerializer(serializers.Serializer):
-    _id = serializers.CharField(allow_blank = True, required = False)
+    id = serializers.CharField(allow_blank=True, required=False)
     temperature = serializers.FloatField()
     date = serializers.DateTimeField()
     city = serializers.CharField(max_length=255, allow_blank=True)
@@ -11,15 +10,5 @@ class WeatherSerializer(serializers.Serializer):
     humidity = serializers.FloatField(required=False)
     weather = serializers.CharField(max_length=255, allow_blank=True)
 
-
     def create(self, validated_data):
         return WeatherEntity(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.temperature = validated_data.get('temperature', instance.temperature)
-        instance.city = validated_data.get('city', instance.city)
-        instance.atmosphericPressure = validated_data.get('atmosphericPressure', instance.atmosphericPressure)
-        instance.humidity = validated_data.get('humidity', instance.humidity)
-        instance.weather = validated_data.get('weather', instance.weather)
-        instance.date = validated_data.get('date', instance.date)
-        return instance
