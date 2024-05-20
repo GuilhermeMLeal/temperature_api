cc82d5d52b967a696c3fc8d199a776b074a0be55
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2024-05-20 00:28:41 -0300

    fix(weather form): updated to dont receive date and to be automatic

--- temperature_api/WeatherForm.py
+++ temperature_api/WeatherForm.py
@@ -1,15 +1,8 @@
-from typing import Any, Mapping
 from django import forms
-from django.forms.renderers import BaseRenderer
-from django.forms.utils import ErrorList
 
 class WeatherForm(forms.Form):
-    temperature = forms.FloatField()
-    date = forms.DateTimeField()
-    city = forms.CharField(max_length=255)
-    atmosphericPressure = forms.FloatField(required=False)
-    humidity = forms.FloatField(required=False)
-    weather = forms.CharField(max_length=255, required=False)
-
-    # def __init__(self, *args, **kwargs):
-    #     self.fields['temperature'].label = 'Temperatura'
+    temperature = forms.FloatField(label='Temperature')
+    city = forms.CharField(label='City', max_length=255)
+    atmosphericPressure = forms.FloatField(label='Atmospheric Pressure', required=False)
+    humidity = forms.FloatField(label='Humidity', required=False)
+    weather = forms.CharField(label='Weather', max_length=255, required=False)
