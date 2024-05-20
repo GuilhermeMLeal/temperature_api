43ee32276f667801fa39dab2713853e9f71e3ba8
GuilhermeMLeal <guilherme.mleal.11@gmail.com>
2024-05-20 00:31:32 -0300

    refactor(auth): changed the manner to be logged

--- user/authenticationUser.py
+++ user/authenticationUser.py
@@ -2,34 +2,42 @@ from .userModel import UserEntity
 import jwt
 from django.conf import settings
 from datetime import datetime, timedelta
-
+from .userRepository import UserRepository
 
 def authenticate(username, password):
-    # regra de autenticação
-    if username == 'user' and password == 'a1b2c3': 
-        # deveria retornar os dados encontrados no banco
-        return UserEntity(username=username)
+    user_repo = UserRepository('users')
+    filter = {'username': username, 'password': password}
+    user_data = user_repo.get(filter)
+    
+    if user_data:
+        user_info = user_data[0]  # Supondo que username seja único
+        return UserEntity(username=user_info['username'], 
+                          name=user_info.get('name'), 
+                          id=user_info.get('id'), 
+                          email=user_info.get('email'), 
+                          password=user_info['password'])
     return None
 
 def generateToken(user):
     payload = {
-        'username' : user.username,
-        'exp' : datetime.utcnow() + timedelta(minutes=10) 
+        'username': user.username,
+        'exp': datetime.utcnow() + timedelta(minutes=10)
     }
-    return jwt.encode(payload = payload, 
-                      key = getattr(settings, 'SECRET_KEY'),
-                      algorithm = 'HS256')
+    return jwt.encode(payload=payload, 
+                      key=getattr(settings, 'SECRET_KEY'),
+                      algorithm='HS256')
 
 def refreshToken(user):
     return generateToken(user)
 
 def verifyToken(token):
     error_code = 0
+    payload = None
 
-    try: 
-        payload = jwt.decode(jwt = token, 
-                      key = getattr(settings, 'SECRET_KEY'),
-                      algorithms = ['HS256'])
+    try:
+        payload = jwt.decode(jwt=token, 
+                             key=getattr(settings, 'SECRET_KEY'),
+                             algorithms=['HS256'])
     except jwt.ExpiredSignatureError:
         error_code = 1
     except jwt.InvalidTokenError:
